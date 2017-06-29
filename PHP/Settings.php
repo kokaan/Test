1983a3974cf59196a5acfcd7fb5203152e285480
kokaan <kokaan@users.noreply.github.com>
2017-06-29 12:26:06 +0800

    Add files via upload

---
+++ PHP/Settings.php
@@ -0,0 +1,10 @@
+<?php
+    class Settings
+    {        
+        public static $appTenantDomainName = 'kokaan88@hotmail.onmicrosoft.com';
+        public static $appPrincipalId = 'eae41fc8-ba86-404b-8475-5b1e50fec97a';    
+        public static $password = '8tOygJT8I7utd5sjJJjrOnS0JjLRUnUtTvU7EBGbPCA=';
+        public static $apiVersion = "api-version=2013-11-08";
+    }
+?>
+
