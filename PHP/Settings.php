d65b0e243cc2b9c91ff4470269197b5c204d9b47
kokaan <kokaan@users.noreply.github.com>
2017-06-29 12:47:10 +0800

    Update Settings.php

--- PHP/Settings.php
+++ PHP/Settings.php
@@ -1,7 +1,7 @@
 <?php
     class Settings
     {        
-        public static $appTenantDomainName = 'kokaan88@hotmail.onmicrosoft.com';
+        public static $appTenantDomainName = 'kokaan88@hotmail.onMicrosoft.com';
         public static $appPrincipalId = '2d5e6111-27b1-4cb5-94c0-d2d9309a8460';    
         public static $password = 'YaQHA2akTgI0wIW1KFDRD5OIOeDNkKNHh1XI21FDKW8=';
         public static $apiVersion = "api-version=2013-11-08";
